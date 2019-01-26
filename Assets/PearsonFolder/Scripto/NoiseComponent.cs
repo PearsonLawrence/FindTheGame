c8563c6a7cc5796950fa6ba92f5db887c55fa808
PearsonLawrence <pearson.lawrence.official@gmail.com>
2019-01-26 11:28:39 +0900

    UI design start

--- Assets/PearsonFolder/Scripto/NoiseComponent.cs
+++ Assets/PearsonFolder/Scripto/NoiseComponent.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 
 public class NoiseComponent : MonoBehaviour
 {
-    public int NoiseLevel;
+    public float NoiseLevel;
 
-    public float ExpandSPeed;
+    public float decreasespeed;
+
+    public float ExpandSpeed;
+
+    public float MaxSize = 30;
 
     public GameObject Owner;
+
+    private SphereCollider spherecollider;
 
     public ParticleSystem SoundWave;
 
     // Start is called before the first frame update
     void Start()
+    {
+        spherecollider = GetComponent<SphereCollider>();
+        spherecollider.radius = 0;
+    }
+
+    //Expands spherecollision and decreases noise level slowly
+    public void ExpandCollision()
+    {
+        spherecollider.radius += Time.deltaTime * ExpandSpeed;
+        NoiseLevel -= Time.deltaTime * decreasespeed;
+
+        if(NoiseLevel <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
     {
         
     }
@@ -21,6 +45,6 @@ public class NoiseComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ExpandCollision();
     }
 }
