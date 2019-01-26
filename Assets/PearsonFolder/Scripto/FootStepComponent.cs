2f8099b76570f026e770c31f115b47b8cf3ac049
PearsonLawrence <pearson.lawrence.official@gmail.com>
2019-01-26 21:19:58 +0900

    started footstepmanager and component

---
+++ Assets/PearsonFolder/Scripto/FootStepComponent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepComponent : MonoBehaviour
+{
+
+    public enum FloorType
+    {
+        Wood,
+        OldWood,
+        HardTile,
+        Carpet
+    }
+
+    public FloorType CurrentFloorType;
+
+    public NoiseComponent StoredNoise;
+
+    public ParticleSystem PS;
+
+    public AudioClip[] FootStepType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void GetFloorType()
+    {
+        RaycastHit hit;
+
+
+
+        Physics.Raycast(transform.position, -transform.up, out hit, 10.0f);
+    }
+
+    public void SpawnFootstep()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/PearsonFolder/Scripto/Misc/FootStepManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepManager : MonoBehaviour
+{
+
+    public float FootStepFrequency;
+
+    private float footStepCounter;
+
+    public GameObject FootStepPrefab;
+
+    float DT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void SpawnFootstep()
+    {
+
+        GameObject Temp = Instantiate(FootStepPrefab, transform.position, Quaternion.identity);
+        //Temp.GetComponent<
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DT = Time.deltaTime;
+
+    }
+}
